8f0171f5f04e6e8f644df1452830b132148e0347
PaaSUser <paasuser@santander.com.br>
2019-02-24 12:29:48 -0300

    first commit

---
+++ index.ts
@@ -0,0 +1,11 @@
+// start:ng42.barrel
+export * from './defaults';
+export * from './files';
+export * from './handlers';
+export * from './meta';
+export * from './tasks';
+export * from './templates';
+export * from './tests';
+export * from './vars';
+// end:ng42.barrel
+
